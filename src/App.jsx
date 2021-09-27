f8f11c104a37b9469c1e42685c3530ec623d22a8
Daniel Sanchez <dsanchez19971234@gmail.com>
2021-09-26 21:23:34 -0500

    new-version

--- src/App.jsx
+++ src/App.jsx
@@ -1,88 +1,10 @@
+import logo from './logo.svg';
+import './App.css';
 
-import "./App.css";
-import { useState, useEffect } from "react";
-import Filter from "./Filter";
-import Header from "./Header";
-import Results from "./Results";
-function App(props) {
-  const [modo, acutalizarModo] = useState(true);
-  const [busqueda, acutalizarBusqueda] = useState("");
-  const [buscarBtn, actualizarBuscarBtn] = useState(false);
-  const [dataGif, actualizarDataGif] = useState([]);
-  const [enviar, actualizarEnviar] = useState("");
-  const [dataAuto, actualizarDataAuto] = useState([]);
-  const manejarModo = () => {
-    acutalizarModo(!modo);
-  };
-  const manejarBusqueda = (e) => {
-    const busqueda = e.target.value;
-    acutalizarBusqueda(busqueda);
-  };
-  const manejarCancelarBusqueda = () => {
-    acutalizarBusqueda("");
-  };
-  const manejarBuscarBtn = (e) => {
-    e.preventDefault();
-    actualizarBuscarBtn(true);
-    actualizarEnviar(busqueda);
-  };
-  useEffect(() => {
-    if (buscarBtn === true) {
-      let key = "dnozkXbIHV5DHw517JitLkUyUmMQpCXb";
-      let urlSearch = "https://api.giphy.com/v1/gifs/search";
-      let petision = fetch(
-        `${urlSearch}?api_key=${key}&q=${busqueda}&limit=12&offset=0&rating=g&lang=en`
-      );
-      petision
-        .then((res) => {
-          return res.json();
-        })
-        .then((data) => {
-          actualizarBuscarBtn(false);
-          acutalizarBusqueda("");
-          actualizarDataGif(data.data);
-        });
-    }
-  }, [buscarBtn, busqueda]);
-  useEffect(() => {
-    let key = "dnozkXbIHV5DHw517JitLkUyUmMQpCXb";
-    let petision = fetch(
-      `https://api.giphy.com/v1/gifs/search/tags?api_key=${key}&q=${busqueda}&limit=5&offset=0&rating=g&lang=en`
-    );
-    petision
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        actualizarDataAuto(data.data);
-      });
-  }, [busqueda]);
+function App() {
   return (
-    <div
-      className={`${
-        modo
-          ? "App flex flex-direction align-items-center"
-          : " App flex flex-direction align-items-center modo-dark "
-      }`}
-    >
-      <Header manejarModo={manejarModo} modo={modo} />
-      <Filter
-        modo={modo}
-        manejarBusqueda={manejarBusqueda}
-        busqueda={busqueda}
-        manejarBuscarBtn={manejarBuscarBtn}
-        dataGif={dataGif}
-        dataAuto={dataAuto}
-        manejarCancelarBusqueda={manejarCancelarBusqueda}
-      />
-      <Results
-        dataGif={dataGif}
-        actualizarDataGif={actualizarDataGif}
-        buscarBtn={buscarBtn}
-        busqueda={busqueda}
-        modo={modo}
-        enviar={enviar}
-      />
+    <div className="App">
+      <h1>COMMIT AND PUSH</h1>
     </div>
   );
 }
